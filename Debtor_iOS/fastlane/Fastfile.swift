8137f3756b126eec19443f234967f8e6ba027c2d
Kerim <ismaisan@mail.ru>
2022-10-30 00:40:36 +0300

    debtorLane

--- Debtor_iOS/fastlane/Fastfile.swift
+++ Debtor_iOS/fastlane/Fastfile.swift
@@ -9,8 +9,8 @@
 import Foundation
 
 class Fastfile: LaneFile {
-	func customLane() {
-	desc("Description of what the lane does")
-		// add actions here: https://docs.fastlane.tools/actions
+	func debtorLane() {
+        desc("Description of what the lane does")
+        scan()
 	}
 }
